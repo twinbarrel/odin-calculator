108e024176504f15e4efabc304af20144258bd29
Andrew Tan <andrewsiqi@gmail.com>
2024-04-07 16:47:19 +0100

    added basic calculations

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function add(a, b) {
-    return a + b;   
+    return parseInt(a) + parseInt(b);   
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a, b) {
@@ -25,7 +25,7 @@ function operate(a, operator, b) {
 }
 
 function isNumber(input) {
-    const nums = [1,2,3,4,5,6,7,8,9]
+    const nums = [0, 1,2,3,4,5,6,7,8,9]
     return nums.includes(parseInt(input));
 }
 
@@ -39,7 +39,7 @@ const buttons = document.querySelector('.display');
 let firstNum = null;
 let operator = null;
 let secondNum = null;
-
+let result = document.querySelector('.result');
 
 buttons.addEventListener("click", function(button){
     // Proceeds only if a button has been clicked
@@ -49,23 +49,60 @@ buttons.addEventListener("click", function(button){
     // Runs different cases based on whether a number, operator or clear is clicked
     let input = button.target.textContent;
     console.log(input + ' ' + isNumber(input) + ' ' + isOperator(input));
-    switch(button.target.textContent) {
+    switch(true) {
         case isNumber(input):
-            // ...;
+            clickNumber(input);
+            break;
         case isOperator(input):
-            // ...'
+            clickOperator(input);
+            break;
+        case (input == '='):
+            clickEquals(input);
+            break;
         case (input == 'C'):
             // ...'
+            break;
     }
 })
 
+function clickNumber(input) {
+    console.log(input);
+    if (!operator) {
+        firstNum = input;
+        secondNum = null;
+    } else if (operator) {
+        secondNum = input;
+    }
+    result.textContent = input;
+}
 
+function clickOperator(input) {
+    console.log(input);
+    if (firstNum >= 0 && operator && secondNum) {
+        console.log(`operating...${firstNum}${operator}${secondNum}`);
+        firstNum = operate(firstNum,operator,secondNum);
+        operator, secondNum = null;
+        result.textContent = firstNum;
+    } else if (!firstNum) {
+        firstNum = 0;
+        operator = input;
+        result.textContent = operator;
+    } else if (firstNum || ! operator) {
+        operator = input;
+        result.textContent = operator;
+    } 
+}
 
-// event listener - when a box ix clicked
-    // Parse textContent of the box
-    // Feed it into a function that processes
+function clickEquals(input) {
+    if (firstNum >= 0 && operator && secondNum) {
+        console.log(`operating...${firstNum}${operator}${secondNum}`);
+        firstNum = operate(firstNum,operator,secondNum);
+        operator, secondNum = null;
+        result.textContent = firstNum;
+    }
+}
 
-/* 
+/*
 
 If a number is clicked:
     If no operator
@@ -74,7 +111,11 @@ If a number is clicked:
         Display the number
     If operator precedes it (+, -, *, /)
         Store as second number
+        Runs operate 
 If an operator is clicked:
+    If first number not present
+        FirstNUm = 0
+        store operator
     If first number is present
         Store operator
     If first number, operator, second number present
